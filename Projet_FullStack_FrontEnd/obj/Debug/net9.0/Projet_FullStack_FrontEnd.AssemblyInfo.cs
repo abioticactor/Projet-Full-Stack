f1cf1abc7209e112e2d92d00d33b6a9cea651be2
Coco <corentinpineau25@gmail.com>
2025-11-07 17:06:50 +0100

    Pré-merge front-back

--- Projet_FullStack_FrontEnd/obj/Debug/net9.0/Projet_FullStack_FrontEnd.AssemblyInfo.cs
+++ Projet_FullStack_FrontEnd/obj/Debug/net9.0/Projet_FullStack_FrontEnd.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Projet_FullStack_FrontEnd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3408385de3c03eac5ee3379410cd36889ed6e17")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8b718505d3267e18375f124f8f9c859857b5a4a8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Projet_FullStack_FrontEnd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Projet_FullStack_FrontEnd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
