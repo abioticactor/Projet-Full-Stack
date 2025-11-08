612d3135680bb19bbc8252efa1ea08de3afc1c81
abioticactor <107043904+abioticactor@users.noreply.github.com>
2025-11-08 09:46:10 +0100

    Nettoyage de l'index Git et ajout du .gitignore

--- PokéDesc.Domain/Dresseur.cs
+++ PokéDesc.Domain/Dresseur.cs
@@ -6,10 +6,10 @@ public class Dresseur
 {
     [BsonId]
     [BsonRepresentation(BsonType.ObjectId)]
-    public string Id { get; set; }
+    public string Id { get; set; } = null!; // On dit à C# que MongoDB va s'en occuper
 
-    public string Pseudo { get; set; }
-    public string Email { get; set; }
-    public string MotDePasseHash { get; set; }
+    public string Pseudo { get; set; } = string.Empty; // On initialise à une chaîne vide
+    public string Email { get; set; } = string.Empty;
+    public string MotDePasseHash { get; set; } = string.Empty;
     // On ajoutera les autres propriétés (niveau, pokemons...) plus tard
 }
